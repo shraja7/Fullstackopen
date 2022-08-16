cb0c351644830a130cf2b8d754489451e276eb26
shraja7 <shraja7@gmail.com>
2022-08-16 15:27:34 -0500

    add comments for explanations

--- part1/courseinfo/src/Course.js
+++ part1/courseinfo/src/Course.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const Course = ({ courses }) => {
   //the main display component, encompasses all the other components that have the infomration
+  //encompasses the course name, the course id, and the parts of the courses and displays them
   const Course = ({ course }) => {
     return (
       <div>
@@ -11,6 +12,7 @@ const Course = ({ courses }) => {
       </div>
     );
   };
+  //the header component, displays the course name
   const Header = ({ course }) => {
     return (
       <div>
@@ -19,6 +21,7 @@ const Course = ({ courses }) => {
     );
   };
 
+  //the content component, displays the parts of the course along with the number of exercises
   const Content = ({ parts }) => {
     return (
       <div>
@@ -28,6 +31,7 @@ const Course = ({ courses }) => {
       </div>
     );
   };
+  //the part component, formatting for each part of the course: name anad exercises
   const Part = ({ part }) => {
     return (
       <div>
@@ -38,11 +42,14 @@ const Course = ({ courses }) => {
     );
   };
 
+  //the total component, displays the total number of exercises in the course
   const Total = ({ parts }) => {
     const total = parts.reduce((acc, part) => acc + part.exercises, 0);
     return <h3>Total: {total}</h3>;
   };
   return (
+    //the course component,  displays the course name and the parts of the course
+    //used map to go through ANY number of courses and pass that information to course component
     <div>
       <h1>Web Development Curriculum</h1>
       {courses.map((course) => (
