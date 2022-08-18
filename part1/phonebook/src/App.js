1f0e99a3282b3124d15eb9314fd72ff5f0cd3ab1
shraja7 <shraja7@gmail.com>
2022-08-18 14:56:36 -0500

    add search filter

--- part1/phonebook/src/App.js
+++ part1/phonebook/src/App.js
@@ -2,10 +2,15 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "12345" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [phone, setPhone] = useState("121456");
   const [newName, setNewName] = useState(" ");
+  const [filter, setFilter] = useState(" ");
+  //const [filter, setFilter] = useState(true);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -33,9 +38,21 @@ const App = () => {
     setPhone(e.target.value);
   };
 
+  const handleFilter = (e) => {
+    setFilter(e.target.value);
+  };
+
+  // const namesToShow = filter
+  // ? persons
+  // : persons.filter(person => person.name === true)
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shows with <input onChange={handleFilter} />
+      </div>
+      <h1>Add New</h1>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input onChange={handleNameChange} />
@@ -48,13 +65,20 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map((person) => (
+
+      {persons
+        .filter((person) => {
+          if (filter === "") {
+            return person;
+          } else if (person.name.toLowerCase().includes(filter.toLowerCase())) {
+            return person;
+          }
+        })
+        .map((person) => (
           <li key={person.name}>
             {person.name} {person.number}
           </li>
         ))}
-      </ul>
     </div>
   );
 };
