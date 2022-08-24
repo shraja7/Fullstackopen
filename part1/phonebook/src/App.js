1aa01d41df40f5741f85148ee49de2356b7a3e39
shraja7 <shraja7@gmail.com>
2022-08-24 08:24:49 -0500

    add useEffect for pulling data from DB

--- part1/phonebook/src/App.js
+++ part1/phonebook/src/App.js
@@ -1,20 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Display from "./Display";
 import Search from "./Search";
 import PersonForm from "./PersonForm";
+import axios from "axios";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+    // { name: "Arto Hellas", number: "040-123456", id: 1 },
+    // { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    // { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    // { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [phone, setPhone] = useState("121456");
   const [newName, setNewName] = useState(" ");
   const [filter, setFilter] = useState(" ");
   //const [filter, setFilter] = useState(true);
 
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
   const handleSubmit = (e) => {
     e.preventDefault();
 
