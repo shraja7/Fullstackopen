e4a85ac39a6abc0e4e97be40844852cac1bf1524
shraja7 <shraja7@gmail.com>
2022-08-26 18:42:17 -0500

    add country information and flags

--- part1/countries/src/SingleCountry.js
+++ part1/countries/src/SingleCountry.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 const SingleCountry = ({ countries }) => {
-  return <div>Single Country: {countries[0].name.common}</div>;
+  return (
+    <div>
+      <h1> {countries[0].name.common}</h1>
+      <h2>Capital: {countries[0].capital}</h2>
+      <h4>Area: {countries[0].area}</h4>
+
+      <h3>
+        {/* iterate over an object */}
+        Languages:
+        {Object.values(countries[0].languages).map((language) => (
+          <p key={language}>{language}</p>
+        ))}
+      </h3>
+      {/* p tag with 100px height and width */}
+
+      <p>{countries[0].flag}</p>
+    </div>
+  );
 };
 
 export default SingleCountry;
