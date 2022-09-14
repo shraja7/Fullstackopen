20c38a84f8277c579f900bea7340eff7f1a643a8
shraja7 <shraja7@gmail.com>
2022-09-13 19:39:08 -0500

    refactor commandline phonebook

--- part1/phonebook/back/mongo.js
+++ part1/phonebook/back/mongo.js
@@ -10,19 +10,22 @@ const password = process.argv[2]
 //take in arguments from CommandLine name and phone number
 //name
 const name = process.argv[3]
-console.log('name:', name)
+
 
 //phone
 const phone = process.argv[4]
-console.log('phone:', phone)
+
 
 const url = `mongodb+srv://shraja7:${password}@cluster0.1fwynkb.mongodb.net/?retryWrites=true&w=majority`
-
+mongoose.connect(url)
 // const noteSchema = new mongoose.Schema({
 //   content: String,
 //   date: Date,
 //   important: Boolean,
 // })
+
+
+
 const personSchema = new mongoose.Schema({
     name: String,
     phone: Number,
@@ -31,21 +34,66 @@ const personSchema = new mongoose.Schema({
   
 const Person = mongoose.model('Person', personSchema)
 
-mongoose
-  .connect(url)
-  .then((result) => {
-    console.log('connected')
+  const person = new Person({
+          name: name,
+      phone: phone
+      
+      })
 
-    const person = new Person({
-        name: name,
-    phone: phone
+
+if(process.argv.length === 3){
+ Person.find({}).then(p =>{
+  console.log(p);
+
+  mongoose.connection.close()
+  process.exit(1)
+ })
+ 
+
+}else{
+  person.save().then(result =>{
+    console.log('person saved')
+    mongoose.connection.close()
+
+  })
+}
+
+
+
+    // mongoose
+    // .connect(url)
+    // .then((result) => {
+    //   console.log('connected')
+
+    //   if(process.argv.length == 3){
+    //     Person.find({}).then(result =>{
+    //       result.forEach(person =>{
+    //        return console.log(person)
+    //       })
+    //       mongoose.connection.close();
+          
+    //     })
+    // }
     
-    })
+    //   const person = new Person({
+    //       name: name,
+    //   phone: phone
+      
+    //   })
+  
+    //   return person.save()
+    // })
+    // .then(() => {
+      
+      
+    //   console.log(`addd ${name} number ${phone} to phonebook`)
+    //   return mongoose.connection.close()
+    // })
+    // .catch((err) => console.log(err))
 
-    return person.save()
-  })
-  .then(() => {
-    console.log(`addd ${name} number ${phone} to phonebook`)
-    return mongoose.connection.close()
-  })
-  .catch((err) => console.log(err))
+
+
+
+
+
+
