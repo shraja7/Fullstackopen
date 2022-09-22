146933c6483bea128ae1287bce94200ab625d831
shraja7 <shraja7@gmail.com>
2022-09-21 20:04:07 -0500

    deploy with validation and add eslint config

--- part1/phonebook/back/index.js
+++ part1/phonebook/back/index.js
@@ -1,28 +1,21 @@
 
 require('dotenv').config()
-const express = require("express");
+const express = require('express')
 // const bodyParser = require('body-parser')
-const morgan = require('morgan');
+const morgan = require('morgan')
 const cors = require('cors')
-const app = express();
-const Person = require('./models/person')
 
+const Person = require('./models/person')
+const app = express()
 app.use(express.json())
-app.use(express.static("build"))
+app.use(express.static('build'))
 
 // app.use(bodyParser.json())
 app.use(cors())
-
-
-
-
-
-
-
 // const person = require("./models/person");
 morgan('tiny')
 
-
+//app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 //create custom morgan token
 morgan.token('body', req => {
   return JSON.stringify(req.body)
@@ -34,51 +27,52 @@ morgan.token('body', req => {
 app.use(morgan(':method :url :body'))
 
 
-app.get("/api/persons", (req, res) => {
- Person.find({}).then(p => {
-  res.json(p)
- })
-});
+app.get('/api/persons', (req, res) => {
+  Person.find({})
+    .then(p => {
+      res.json(p)
+    }).catch(e => next(e))
+})
 
-app.get("/api/persons/info", (req, res) => {
-  
+app.get('/api/persons/info', (req, res) => {
+
   Person.find({}).then(p => {
-   res.send(`Phonebook has ${p.length} entries
+    res.send(`Phonebook has ${p.length} entries
    <p> ${Date()}</p>`)
   })
- });
+})
 //funcitonality for displaying information for a single person entry
-app.get("/api/persons/:id", (request, response,next) => {
+app.get('/api/persons/:id', (request, response,next) => {
   // const id = Number(request.params.id);
   // const person = persons.find((p) => p.id === id);
-Person.findById(request.params.id).then(p =>{
-  response.json(p)
-
-}).then( p => {
-  if(p){
+  Person.findById(request.params.id).then(p => {
     response.json(p)
-  }else{
-    response.status(404).end()
-  }
- 
-}
-).catch(error => next(error))
 
-});
+  }).then( p => {
+    if(p){
+      response.json(p)
+    }else{
+      response.status(404).end()
+    }
+
+  }
+  ).catch(error => next(error))
+
+})
 //info route to display information of the backend
 
 //funcitonality for deleting a specific person based on ID
-app.delete("/api/persons/:id", (request, response,next)=>{
+app.delete('/api/persons/:id', (request, response,next) => {
 
 
   Person.findByIdAndRemove(request.params.id)
-  .then(() =>{
-    response.status(204).end()
-  }).catch(error => next(error))
+    .then(() => {
+      response.status(204).end()
+    }).catch(error => next(error))
 
 })
 //add funcitonality so that new phonebook entries can be added by making post requests
-app.post('/api/persons',(request, response, next)=>{
+app.post('/api/persons',(request, response, next) => {
   // Implement error handling for creating new entries. The request is not allowed to succeed, if:
 
   //   The name or number is missing --DONE
@@ -86,7 +80,7 @@ app.post('/api/persons',(request, response, next)=>{
 
   const body = request.body
 
-//check if person already exists
+  //check if person already exists
   // const alreadyExists = persons.find(person => person.name === body.name)
   // console.log(alreadyExists);
   // if(alreadyExists){
@@ -101,41 +95,41 @@ app.post('/api/persons',(request, response, next)=>{
     })
   }
 
-const person = new Person({
-  name: body.name.trim(),
-  number: body.number.trim(),
-})
+  const person = new Person({
+    name: body.name.trim(),
+    number: body.number.trim(),
+  })
   // persons = persons.concat(person)
 
 
   // response.json(person)
   person
-  .save()
-  .then(savedPerson =>{
-    response.json(savedPerson.toJSON())
-  })
-   .catch(error => next(error))
+    .save()
+    .then(savedPerson => {
+      response.json(savedPerson.toJSON())
+    })
+    .catch(error => next(error))
 
 })
-//If the user tries to create a new phonebook 
-//entry for a person whose name is already in the phonebook, the frontend 
-//will try to update the phone number of the existing entry by 
+//If the user tries to create a new phonebook
+//entry for a person whose name is already in the phonebook, the frontend
+//will try to update the phone number of the existing entry by
 //making an HTTP PUT request to the entry's unique URL.
-app.put('/api/persons/:id', (request, response, next)=>{
-  
-const body = request.body
-const person = {
-  name: body.name,
-  number: body.number,
-}
+app.put('/api/persons/:id', (request, response, next) => {
+
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
 
 
 
-  Person.findByIdAndUpdate(request.params.id, person, {new: true, runValidators: true, context: 'query'})
-  .then(updatedPerson =>{
-    response.json(updatedPerson)
-  })
-   .catch(error => next(error))
+  Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 //error handler
@@ -157,7 +151,7 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running at ${PORT}`);
-});
+  console.log(`Server running at ${PORT}`)
+})
--- part1/phonebook/back/models/person.js
+++ part1/phonebook/back/models/person.js
@@ -5,37 +5,38 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
+
 //connect to mongoDB
 mongoose.connect(url )
   .then(() => {
-        console.log('connected to MongoDB')  })  
-        .catch((error) => {    console.log('error connecting to MongoDB:', error.message) 
-     })
+    console.log('connected to MongoDB')  })
+  .catch((error) => {    console.log('error connecting to MongoDB:', error.message)
+  })
 
-     const personSchema = new mongoose.Schema({
-      name: { type: String, required: true,  minLength: [3, 'Name must have atleast 3 characters'] },
-      // number: { type: String, required: true,  minLength: [8, 'Number must have atleast 8 characters'] },
-      number: {
-        type: String, 
-        validate: {
-          validator: function(v){
-            return /\d{3}-\d{3}-\d{4}/.test(v);
-          },
-          message: props => `${props.value} is not a valid phone number!`
-        },
-        required: [true, 'User phone number required']
-      
-      }
-    })
-      //modify schema to return a certain way
-      //Even though the _id property of Mongoose objects looks like a string, it is in fact an object.
- personSchema.set('toJSON', {
-        transform: (document, returnedObject) => {
-          returnedObject.id = returnedObject._id.toString()
-          delete returnedObject._id
-          delete returnedObject.__v
-        }
-      })
+const personSchema = new mongoose.Schema({
+  name: { type: String, required: true,  minLength: [3, 'Name must have atleast 3 characters'] },
+  // number: { type: String, required: true,  minLength: [8, 'Number must have atleast 8 characters'] },
+  number: {
+    type: String,
+    validate: {
+      validator: function(v){
+        return /\d{3}-\d{3}-\d{4}/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
+
+  }
+})
+//modify schema to return a certain way
+//Even though the _id property of Mongoose objects looks like a string, it is in fact an object.
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
 
 module.exports = mongoose.model('Person', personSchema)
