6bfa76b41a339c551a8f36ca90aa08a30aa8c3c1
shraja7 <shraja7@gmail.com>
2022-09-29 01:02:44 -0500

    add favoriteBlog test to find blog with most likes

--- part4/bloglist/back/utils/list_helper.js
+++ part4/bloglist/back/utils/list_helper.js
@@ -13,7 +13,18 @@ const totalLikes =(blogs)=>{
 
 }
 
+//find which blog has most likes THEN return that blog object
+const favoriteBlog = (blogs) => { 
+ //find the most likes out of all the blogs
+ let mostLikes = Math.max(...blogs.map(blog => blog.likes))
+ //find which object in that array of blogs has likes = mostLikes
+ let mostLikedBlog = blogs.find(blog => blog.likes === mostLikes)
+
+ return mostLikedBlog
+ }
+
+
 
 module.exports ={
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog
 }
