5426cf9c7db06ad81fa5a3d0b87fec96c8717346
shraja7 <shraja7@gmail.com>
2022-10-25 20:49:37 -0500

    add style to individual blogs

--- part4/bloglist/front/src/components/Blog.js
+++ part4/bloglist/front/src/components/Blog.js
@@ -1,7 +1,25 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
+const Blog = ({blog}) => {
+  //styyle for blog
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+
+  
+return( 
+<div style={blogStyle}>
+    <div>{blog.title} {blog.author}</div>
+    <div>{blog.url}</div>
+    <div>{blog.likes} likes <button>like</button></div>
+    
+    
   </div>  
 )
+}
 
 export default Blog
